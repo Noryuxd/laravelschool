03165016baf644028908fad231c7d7821294acc0
Noryu <rabiiababsa10@gmail.com>
2023-11-28 09:48:13 +0100

    TP1 added

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,14 @@
 <?php
 
 use App\Http\Controllers\TestController;
+use App\Http\Controllers\Tp1Controller;
 use Illuminate\Support\Facades\Route;
 
-Route::get("/", function(){
+Route::get("/", function () {
     return view("welcome");
 });
 
 Route::get('/test', [TestController::class, "test"]);
 
-Route::get('/{id}', function (int $id) {
-    return $id;
-});
+Route::get('/tp1', [Tp1Controller::class, "get"]);
+Route::post('/tp1', [Tp1Controller::class, "post"])->name("tp1");
