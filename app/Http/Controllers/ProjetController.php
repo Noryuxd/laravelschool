0ac15c36bb1dab9bccd5ce0d5d81086fbb7d96cd
Noryu <rabiiababsa10@gmail.com>
2024-01-09 10:57:23 +0100

    construct auth test

--- app/Http/Controllers/ProjetController.php
+++ app/Http/Controllers/ProjetController.php
@@ -8,6 +8,10 @@ use Illuminate\Http\Request;
 
 class ProjetController extends Controller
 {
+  public function __construct()
+  {
+    $this->middleware('auth');
+  }
   /**
    * Display a listing of the resource.
    */
@@ -45,7 +49,6 @@ class ProjetController extends Controller
 
   public function show(Projet $projet)
   {
-    
   }
 
   /**
