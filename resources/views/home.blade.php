128efa0aada75ab840e9c8d7a069112f29c2f568
Noryu <rabiiababsa10@gmail.com>
2024-01-09 10:21:16 +0100

    Dashboard added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+    <div class="container p-4">
+        <div class="row">
+            <div class="col-md-4 mb-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h2 class="card-title">Gestion d'employés</h2>
+                        <p class="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis, dicta.</p>
+                        <a href="{{ url('/TP6') }}" class="btn btn-primary">Liste d'employés</a>
+                        <a href="{{ url('/tp-5') }}" class="btn btn-success">Nouveau employé</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h2 class="card-title">Gestion de projets</h2>
+                        <p class="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis, dicta.</p>
+                        <a href="{{ url('/TP6') }}" class="btn btn-primary">Liste de projets</a>
+                        <a href="{{ url('/TP6/create') }}" class="btn btn-success">Nouveau projet</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h2 class="card-title">Gestion de taches</h2>
+                        <p class="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis, dicta.</p>
+                        <a href="{{ url('/tache') }}" class="btn btn-primary">Liste des taches</a>
+                        <a href="{{ url('/tache/create') }}" class="btn btn-success">Nouvelle tache</a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
